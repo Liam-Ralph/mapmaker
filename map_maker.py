8f2db3ababb086cd185ab7332ad79def7a010852
Liam-Ralph <liamralph42@gmail.com>
2024-12-31 17:09:33 -0500

    fix pool usage

--- map_maker.py
+++ map_maker.py
@@ -21,6 +21,7 @@ class Dot:
 class TreeManager(multiprocessing.managers.BaseManager):
     pass
 
+
 # Text Colors
 
 ANSI_GREEN = "\u001b[38;5;2m"
@@ -75,8 +76,8 @@ def raise_error(location, traceback_output, notes = None):
 
 def initialize_pool(section_progress_value,
 dots_value, width_value, height_value, processes_value, start_time_value,
-tree_value, pixels_value,
-island_abundance_value, island_size_value, relative_island_abundance_value):
+island_abundance_value, island_size_value, relative_island_abundance_value,
+image_sections_value, lock_value):
     
     global section_progress
     global dots
@@ -84,12 +85,12 @@ island_abundance_value, island_size_value, relative_island_abundance_value):
     global height
     global processes
     global start_time
-    global tree
-    global pixels
     global island_abundance
     global island_size
     global relative_island_abundance
     global current_section_progress
+    global image_sections
+    global lock
 
     section_progress = section_progress_value
     dots = dots_value
@@ -97,11 +98,11 @@ island_abundance_value, island_size_value, relative_island_abundance_value):
     height = height_value
     processes = processes_value
     start_time = start_time_value
-    tree = tree_value
-    pixels = pixels_value
     island_abundance = island_abundance_value
     island_size = island_size_value
     relative_island_abundance = relative_island_abundance_value
+    image_sections = image_sections_value
+    lock = lock_value
 
 def track_progress(section_progress, section_progress_total, start_time, section_times):
 
@@ -110,7 +111,7 @@ def track_progress(section_progress, section_progress_total, start_time, section
         while True:
 
             clear_screen()
-            progress_total = 0
+            total_progress = 0
 
             # Section Progress
 
@@ -124,38 +125,44 @@ def track_progress(section_progress, section_progress_total, start_time, section
                 if section_total == 0:
                     section_total = 1
                 progress_section = section_progress[i] / section_total
-                section_times[i] = time.time() - start_time.value
-                section_times[i] -= sum(section_times[ii] for ii in range(i))
-                progress_total += progress_section * section_weights[i]
+                total_progress += progress_section * section_weights[i]
 
-                if progress_section == 1.0:
+                if section_progress[i] == section_progress_total[i]:
                     color = ANSI_GREEN
                 else:
+                    section_times[i] = time.time() - start_time.value
+                    section_times[i] -= sum(section_times[ii] for ii in range(i))
                     color = ANSI_BLUE
                 print(
                     color + "[" + str(i + 1) + "/4] " + section_names[i].ljust(20) +
                     "{:.2f}% ".format(progress_section * 100).rjust(8) +
-                    ANSI_GREEN + "█" * int(progress_section * 20) +
-                    ANSI_BLUE + "█" * (20 - int(progress_section * 20)) +
+                    ANSI_GREEN + "█" * round(progress_section * 20) +
+                    ANSI_BLUE + "█" * (20 - round(progress_section * 20)) +
                     ANSI_RESET + " " + format_time(section_times[i])
                 )
 
             # Total Progress
-            progress_total /= 0.35 # remove later
+            total_progress /= 0.35 # remove later
 
-            if progress_total == 1.0:
+            if sum(section_progress) == sum(section_progress_total):
                 color = ANSI_GREEN
             else:
                 color = ANSI_BLUE
             print(
                 color + "      Total Progress      " +
-                "{:.2f}% ".format(progress_total * 100).rjust(8) +
-                ANSI_GREEN + "█" * int(progress_total * 20) +
-                ANSI_BLUE + "█" * (20 - int(progress_total * 20)) +
+                "{:.2f}% ".format(total_progress * 100).rjust(8) +
+                ANSI_GREEN + "█" * round(total_progress * 20) +
+                ANSI_BLUE + "█" * (20 - round(total_progress * 20)) +
                 ANSI_RESET + " " + format_time(time.time() - start_time.value)
             )
 
-            if progress_total == 1.0:
+            print(str(section_progress[0]))
+            print(str(section_progress_total[0]))
+            print(str(section_progress[3]))
+            print(str(section_progress_total[3]))
+
+            if (sum(section_progress) == sum(section_progress_total) and not
+            all(value > 0 for value in section_progress)) and section_progress[3] > 0: # remove later
                 break
             else:
                 time.sleep(0.1)
@@ -163,54 +170,69 @@ def track_progress(section_progress, section_progress_total, start_time, section
     except:
         raise_error("track_progress", traceback.format_exc())
 
-def generate_sections(coord):
+def generate_sections(coords):
 
     try:
 
-        dot_type = "Water"
-        if random.randint(1, relative_island_abundance.value) == 1:
-            dot_type = "Land Origin"
-        elif random.randint(1, (relative_island_abundance.value - 1)) == 1:
-            dot_type = "Water Forced"
-        dots.append(Dot(coord % width.value, coord // width.value, dot_type))
+        for coord in coords:
 
-        section_progress[0] += 1
+            dot_type = "Water"
+            if random.randint(1, relative_island_abundance.value) == 1:
+                dot_type = "Land Origin"
+            elif random.randint(1, (relative_island_abundance.value - 1)) == 1:
+                dot_type = "Water Forced"
+            dots.append(Dot(coord % width.value, coord // width.value, dot_type))
 
+            with lock:
+                section_progress[0] += 1
 
     except:
         raise_error(
-            "generate_sections", traceback.format_exc(), notes = ("Input: " + str(coord), )
+            "generate_sections", traceback.format_exc(), notes = ("Input: " + str(coords), )
         )
 
-def generate_image(x, y):
+def generate_image(start_height, section_height, process_num):
 
     try:
 
-        pixel_type = "Error"
-        pixel_type = dots[tree.query([(x, y)])[1][0]].type
+        image_local = (
+            PIL.Image.new("RGB", (width.value, section_height), (255, 153, 194))
+        )
+        pixels = image_local.load()
+        dot_coords = []
+        for dot in dots:
+            dot_coords.append((dot.x, dot.y))
+        tree = scipy.spatial.KDTree(dot_coords)
 
-        match (pixel_type):
-            case "Land":
-                pixels[y][x] = (0, 204, 0)
-            case "Land Origin":
-                pixels[y][x] = (0, 102, 0)
-            case "Water":
-                pixels[y][x] = (0, 0, 204)
-            case "Water Forced":
-                pixels[y][x] = (0, 0, 102)
-            case "Error":
-                pixels[y][x] = (255, 102, 163)
-            case _:
-                pixels[y][x] = (204, 0, 82)
+        for y in range(section_height):
+            for x in range(width.value):
 
+                pixel_type = "Error"
+                pixel_type = dots[tree.query([(x, y + start_height)])[1][0]].type
 
-        section_progress[3] += 1
+                match (pixel_type):
+                    case "Land":
+                        pixels[x, y] = (0, 204, 0)
+                    case "Land Origin":
+                        pixels[x, y] = (0, 102, 0)
+                    case "Water":
+                        pixels[x, y] = (0, 0, 204)
+                    case "Water Forced":
+                        pixels[x, y] = (0, 0, 102)
+                    case "Error":
+                        pixels[x, y] = (255, 102, 163)
+                    case _:
+                        pixels[x, y] = (204, 0, 82)
 
+                with lock:
+                    section_progress[3] += 1
+
+        image_sections[process_num] = image_local
 
     except:
         raise_error(
             "generate_image", traceback.format_exc(),
-            notes = ("Input \"x\": " + str(x), "Input \"y\": " + str(y))
+            notes = ("Input: " + str(section_height), )
         )
 
 
@@ -264,10 +286,8 @@ if __name__ == "__main__":
         "Number of Threads:"
     )
     processes_input = get_int(1, 32)
-    if processes_input > 1:
-        processes_input -= 1
 
-    clear_screen
+    clear_screen()
 
     TreeManager.register("Tree", scipy.spatial.KDTree)
 
@@ -283,14 +303,15 @@ if __name__ == "__main__":
     processes = multiprocessing.Value(ctypes.c_int, processes_input)
     start_time = multiprocessing.Value(ctypes.c_double, time.time())
     section_times = multiprocessing.Array(ctypes.c_double, [0.0, 0.0, 0.0, 0.0])
-    tree = tree_manager.Tree([(0, 0), (1, 1)])
-    pixels = manager.list([[(255, 0, 102)] * width.value] * height.value)
     island_abundance = multiprocessing.Value(ctypes.c_int, island_abundance_input)
     island_size = multiprocessing.Value(ctypes.c_int, island_size_input)
     relative_island_abundance = multiprocessing.Value(ctypes.c_int, relative_island_abundance_input)
+    image_sections = manager.list([PIL.Image.new("RGB", (100, 100), (255, 0, 102))] * processes.value)
+    lock = multiprocessing.Lock()
 
     with multiprocessing.Pool(processes_input, initializer = initialize_pool, initargs =
-    (section_progress, dots, width, height, processes, start_time, tree, pixels, island_abundance, island_size, relative_island_abundance)) as pool:
+    (section_progress, dots, width, height, processes, start_time,
+    island_abundance, island_size, relative_island_abundance, image_sections, lock)) as pool:
 
         try:
 
@@ -305,37 +326,41 @@ if __name__ == "__main__":
             num_dots = width.value * height.value // island_abundance.value
             section_progress_total[0] = num_dots
             results = []
-            for coord in random.sample(range(0, width.value * height.value), num_dots):
-                results.append(pool.apply_async(generate_sections, (coord, )))
+            coords = random.sample(range(0, width.value * height.value), num_dots)
+            coord_sections = [coords[i : i + num_dots // processes.value] for i in range(0, num_dots, num_dots // processes.value)]
+            if num_dots % processes.value != 0:
+                coord_sections[-2].extend(coord_sections[-1])
+                coord_sections.pop()
+            for i in range(processes.value):
+                results.append(pool.apply_async(generate_sections, (coord_sections[i], )))
             [result.wait() for result in results]
-
-            dot_coords = []
-            for dot in dots:
-                dot_coords.append((dot.x, dot.y))
-            tree.data = dot_coords
 
             # Image Generation
 
             section_progress_total[3] = width.value * height.value
             results = []
-            for i in range(width.value * height.value):
-                results.append(pool.apply_async(generate_image,
-                    (i % width.value, i // width.value, width.value * height.value)))
+            start_heights = list(range(0, height.value, height.value // processes.value))
+            section_heights = [height.value // processes.value] * (processes.value - 1)
+            section_heights.append(height.value - sum(section_heights))
+            for i in range(processes.value):
+                results.append(pool.apply_async(generate_image, (start_heights[i], section_heights[i], i)))
             [result.wait() for result in results]
 
             tracker_process.join()
 
+            # Image Stitching
+
+            image = PIL.Image.new("RGB", (width.value, height.value), (255, 51, 133))
+            shift = 0
+            for section in image_sections:
+                image.paste(section, (0, shift))
+                shift += section_heights[0]
+            image.save("result.png")
+
         except:
             raise_error("Pool Parent Process", traceback.format_exc())
-    
+
     print(
         ANSI_GREEN + "Generation Complete " + ANSI_RESET +
         format_time(time.time() - start_time.value)
     )
-
-    image = PIL.Image.new("RGB", (width.value, height.value), (255, 51, 133))
-    image_pixels = image.load()
-    for y in range(height.value):
-        for x in range(width.value):
-            image_pixels[x, y] = pixels[y][x]
-    image.save("result.png")
